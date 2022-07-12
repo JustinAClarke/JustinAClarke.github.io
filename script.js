4567d15fa3f1332d6c70324b4acebc53d241b284
Justin Clarke <justin.clarke@schoolbox.com.au>
2022-07-13 08:10:29 +1000

    update script to dynamically get updated repos

--- script.js
+++ script.js
@@ -21,13 +21,13 @@
     function addRepos(repos, page) {
         repos = repos || [];
         page = page || 1;
-        var uri = 'https://api.github.com/users/justinfuhrmeister-clarke/repos?callback=?' + '&per_page=100' + '&page=' + page;
+        var uri = 'https://api.github.com/users/JustinAClarke/repos?per_page=100&page=1';
         // var uri = '/repos.json';
         $.getJSON(uri, function(result) {
             // API Rate limiting catch
             if (result.data && result.data.message  || !result.data) {
                 $('<p class="alert alert-warning">').text('Your IP has hit github\'s rate limit per hour.').appendTo('#error');
-                $('<a href="https://github.com/JustinFuhrmeister-Clarke/repositories">').text('Please click here to view all Repos').appendTo('#error');
+                $('<a href="https://github.com/JustinAClarke/repositories">').text('Please click here to view all Repos').appendTo('#error');
                 $('#error').show();
                 $('#loading').hide();
 
